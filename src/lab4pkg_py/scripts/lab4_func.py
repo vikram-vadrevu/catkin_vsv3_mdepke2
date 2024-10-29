f1c5e8f4d17f3b5bfdc86b8380fcf8042fb5b610
Lab Computer <ur3@ur3.com>
2024-10-28 21:21:17 -0500

    Finished lab 4

--- src/lab4pkg_py/scripts/lab4_func.py
+++ src/lab4pkg_py/scripts/lab4_func.py
@@ -8,112 +8,80 @@ Use 'expm' for matrix exponential.
 Angles are in radian, distance are in meters.
 """
 def Get_MS():
-	# =================== Your code starts here ====================#
-	# Fill in the correct values for a1~6 and q1~6, as well as the M matrix
-	#M = np.eye(4)
-	#S = np.zeros((6,6))
-	#M is relative to world frame
-	M = np.array([[0,-1,0,390],[0,0,-1,401],[1,0,0,215.5],[0,0,0,1]])
-		#find the S matricies
-	#S1
-	w1 = np.array([ 0, 0,1 ])
-	q1 = np.array([ -150, 150, 162])
-	v1 = np.cross(-w1,q1)
-	s1 = np.append(w1,v1)
+   # =================== Your code starts here ====================#
+   # Fill in the correct values for S1~6, as well as the M matrix
 
-	#S2
-	w2 = np.array([0 ,1 ,0 ])
-	q2 = np.array([ -150,270 , 162])
-	v2 = np.cross(-w2,q2)
-	s2 = np.append(w2,v2)
-	#S3
-	w3 = np.array([ 0, 1,0 ])
-	q3 = np.array([ 94, 240, 162])
-	v3 = np.cross(-w3,q3)
-	s3 = np.append(w3,v3)
-	#S4
-	w4 = np.array([ 0, 1,0 ])
-	q4 = np.array([ 307,177 ,162 ])
-	v4 = np.cross(-w4,q4)
-	s4 = np.append(w4,v4)
-	#S5
-	w5 = np.array([1 ,0 ,0 ])
-	q5 = np.array([ 307,260 ,162 ])
-	v5 = np.cross(-w5,q5)
-	s5 = np.append(w5,v5)
-	#S3
-	w6 = np.array([ 0, 1, 0])
-	q6 = np.array([390 , 260, 162])
-	v6 = np.cross(-w6,q6)
-	s6 = np.append(w6,v6)
-	S = np.array([s1,s2,s3,s4,s5,s6])   # might need to trannypose ?
 
-	l1 = 152
-	l2 = 120
-	l3 = 244
-	l4 = 93
-	l5 = 213
-	l6 = 83
-	l7 = 83
-	l8  = 82
-	l9 = 53.5
-	l10 = 59
-	
-	###########################
+   P_BS = np.array([(540-150)/1000, (120-93+83+82+59+150)/1000, (10+152+53.5)/1000])
+   M  = np.array([[0,-1, 0, P_BS[0]],
+                  [0, 0,-1, P_BS[1]],
+                  [1, 0, 0, P_BS[2]],
+                  [0, 0, 0, 1]])
+  
+   # home_offset = np.array([180, 0, 0, -90, 0, 0])
+
+
+   # S = np.array([[0,0,0,0,1,0],
+   #             [0,1,-1,1,0,1],
+   #             [1,0,0,0,0,0],
+   #             [0.15,-0.162,0.162,-0.162,0,-0.162],
+   #             [0.15,0,0,0,0.162,0],
+   #             [0,-0.15,-0.094,0.307,-0.26,0.39]])
+
+
+#    V_1 = np.cross(-np.array([[0,0,1]]), np.array([[-150, 150, 10]]))/1000
+#    V_2 = np.cross(-np.array([[0, 1, 0]]), np.array([[-150, 150+120, 10+152]]))/1000
+#    V_3 = np.cross(-np.array([[0,1,0]]), np.array([[-150+244, 150+120, 10+152]]))/1000
+#    V_4 = np.cross(-np.array([[0, 1, 0]]), np.array([[-150+244+213, 150+120-93, 10+152]]))/1000
+#    V_5 = np.cross(-np.array([[1, 0, 0]]), np.array([[-150+244+213, 150+120-93+83, 10+152]]))/1000
+#    V_6 = np.cross(-np.array([[0, 1, 0]]), np.array([[-150+540, 150+120-93+83, 10+152]]))/1000
+    
+
+   S = np.array([[0,0,1,0.15,0.15,0],
+                 [0,1,0,-0.162,0,-0.15],
+                 [0,1,0,-0.162,0,0.094],
+                 [0,1,0,-0.162,0,0.307],
+                 [1,0,0,0,0.162,-0.26],
+                 [0,1,0,-0.162,0,0.39]]).T
+    
+#    S[3:,0] =  np.cross(-np.array([[0,0,1]]), np.array([[-150, 150, 10]]))/1000
+#    S[3:,1]  = np.cross(-np.array([[0, 1, 0]]), np.array([[-150, 150+120, 10+152]]))/1000
+#    S[3:,2]  = np.cross(-np.array([[0,1,0]]), np.array([[-150+244, 150+120, 10+152]]))/1000
+#    S[3:,3]  = np.cross(-np.array([[0, 1, 0]]), np.array([[-150+244+213, 150+120-93, 10+152]]))/1000
+#    S[3:,4]  = np.cross(-np.array([[1, 0, 0]]), np.array([[-150+244+213, 150+120-93+83, 10+152]]))/1000
+#    S[3:,5]  = np.cross(-np.array([[0, 1, 0]]), np.array([[-150+540, 150+120-93+83, 10+152]]))/1000
+
+   return M, S
 
 
 
 
-
-
-
-
-
-
-
-
-
-	# ==============================================================#
-	return M, s1,s2,s3,s4,s5,s6
-
-
-"""
-Function that calculates encoder numbers for each motor
-"""
 def lab_fk(theta1, theta2, theta3, theta4, theta5, theta6):
+   """
+   Function that calculates encoder numbers for each motor
+   """
+   # Initialize the return_value
+   return_value = np.array([theta1, theta2, theta3, theta4, theta5, theta6])
+   theta = return_value
+   # =========== Implement joint angle to encoder expressions here ===========
+   print("Foward kinematics calculated:\n")
 
-	# Initialize the return_value
-	return_value = [None, None, None, None, None, None]
-
-	print("Foward kinematics calculated:\n")
-
-
-	# =================== Your code starts here ====================#
-	theta = np.array([theta1,theta2,theta3,theta4,theta5,theta6])
-	T = np.eye(4)
-	
-	M,s1,s2,s3,s4 ,s5,s6 = Get_MS()
-
-	T = expm(VecTose3(s1)*theta1) @ expm(VecTose3(s2)*theta2)@ expm(VecTose3(s3)*theta3) @ expm(VecTose3(s4)*theta4) @ expm(VecTose3(s5)*theta5) @ expm(VecTose3(s6)*theta6) @ M
+   M, S = Get_MS()
+   T = FKinSpace(M, S, theta)
 
 
-
-	# ==============================================================#
-
-	return_value[0] = theta1 + PI
-	return_value[1] = theta2
-	return_value[2] = theta3
-	return_value[3] = theta4 - (0.5*PI)
-	return_value[4] = theta5
-	return_value[5] = theta6
-	print(T)
-	return return_value
+   print(str(T) + "\n")
 
 
+   return_value[0] = theta1 + PI
+   return_value[1] = theta2
+   return_value[2] = theta3
+   return_value[3] = theta4 - (0.5*PI)
+   return_value[4] = theta5
+   return_value[5] = theta6
 
 
-	# ==============================================================#
-
+   return return_value
 
 
 
@@ -127,8 +95,8 @@ def lab_invk(xWgrip, yWgrip, zWgrip, yaw_WgripDegree):
 	zWgrip = zWgrip - .010
 	links = np.array([-1000, 152, 120, 244, 93, 213, 83, 83, 82, 53.5, 59])/1000
 	print(f'Links: {links}')
-	x_center = xWgrip - 0.0535*np.sin(np.radians(yaw_WgripDegree))
-	y_center = yWgrip - 0.0535*np.cos(np.radians(yaw_WgripDegree))
+	x_center = xWgrip - links[9]*np.cos(np.radians(yaw_WgripDegree))
+	y_center = yWgrip - links[9]*np.sin(np.radians(yaw_WgripDegree))
 	z_center = zWgrip
 
 	theta1 = np.arctan2(y_center, x_center) - np.arcsin((0.110) / np.sqrt(x_center**2 + y_center**2))
@@ -148,19 +116,14 @@ def lab_invk(xWgrip, yWgrip, zWgrip, yaw_WgripDegree):
 	x_3end, y_3end = pos_3end.reshape(2,).tolist()
 	z_3end = z_center + links[10] + links[8]
 
-	# c_line = np.sqrt((x_3end - (-0.150))**2 + (z_3end - (0.010 + links[1]))**2)
-	c_line = np.sqrt((x_3end - 0)**2 + (y_3end - 0)**2 + (z_3end - links[1])**2) # not ours PLEASE REMOVE
 	c_line = np.sqrt(x_3end**2 + y_3end**2 + (z_3end - links[1])**2)
-	print(f'Cl_line: {c_line}')
 
 	theta3 = (np.pi) - (np.arccos((links[5]**2 + links[3]**2 - c_line**2)/(2*links[3]*links[5])))
-	print(f'ratio: {(links[5]**2 + links[3]**2 - c_line**2)/(2*links[3]*links[5])}')
 
 	theta2small = np.arccos(np.sqrt(x_3end**2 + y_3end**2)/c_line)
 
-	theta2big = -1 * np.arccos((links[3]**2 + c_line**2 - links[5]**2) / (2*links[3]*c_line))
+	theta2big = np.arccos((links[3]**2 + c_line**2 - links[5]**2) / (2*links[3]*c_line))
 	
-	#theta2 = theta2small + theta2big
 	theta2 = -theta2small - theta2big
 	theta4 = -theta2 - theta3
 	theta5 = -np.pi/2
